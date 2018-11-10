8fa25a62fd91525b07ddce476f63523ea06fe469
jskno <lodmnos@gmail.com>
2018-11-10 13:24:42 +0100

    Starting Back End

---
+++ managinglists-be/src/main/java/com/jskno/managinglistsbe/ManaginglistsBeApplication.java
@@ -0,0 +1,12 @@
+package com.jskno.managinglistsbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManaginglistsBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManaginglistsBeApplication.class, args);
+    }
+}
